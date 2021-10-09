35db8303892a6147dcc15b928918493c08777b37
mrhappyasthma <mrhappyasthma@gmail.com>
2021-10-09 16:55:23 -0700

    Fix type in import.

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import { Server } from "@ravens-engine/core/lib/server/index.js";
-import CoupGame from "./CoupGame";
+import CoupGame from "./CoupGame.js";
 
 const server = new Server({
   gameClass: CoupGame
