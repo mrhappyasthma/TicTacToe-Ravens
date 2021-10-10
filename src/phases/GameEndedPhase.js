64cf4897153862b35d6565592d82d941e61c9606
mrhappyasthma <mrhappyasthma@gmail.com>
2021-10-10 09:05:04 -0700

    Implement a simple GameEndedPhase
    This phase tracks the winner so that the UI can reflect it.

--- src/phases/GameEndedPhase.js
+++ src/phases/GameEndedPhase.js
@@ -1,5 +1,11 @@
 import { Phase } from "@ravens-engine/core/lib/core/index.js";
 
-export class GameEndedPhase extends Phase { }
+export class GameEndedPhase extends Phase {
+  initialize(winner) {
+    this.state = {
+      winner
+    }
+  }
+}
 
 GameEndedPhase.id = "game-ended";
