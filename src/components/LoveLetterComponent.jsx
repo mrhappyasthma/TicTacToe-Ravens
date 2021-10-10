9133016cc4775b925d4f1c0c6ca2969cf6fcd9bc
mrhappyasthma <mrhappyasthma@gmail.com>
2021-10-10 10:07:03 -0700

    Re-implement the grid game rendering

--- src/components/LoveLetterComponent.jsx
+++ src/components/LoveLetterComponent.jsx
@@ -13,7 +13,7 @@ export default class LoveLetterComponent extends React.Component {
 
       <table>
 		    <tbody>
-
+          {this.renderGameGrid()}
 		    </tbody>
       </table>
     </>;
@@ -24,17 +24,33 @@ export default class LoveLetterComponent extends React.Component {
       return <>Waiting for <b>{2 - this.props.game.players.length}</b> more player(s)...</>;
     }
     if (this.props.game.child instanceof GameInProgressPhase) {
-      let gameInProgressPhase = this.props.game.child;
+      const gameInProgressPhase = this.props.game.child;
       return <>Turn: <b>{gameInProgressPhase.state.turn}</b> {gameInProgressPhase.isTurn(this.props.client.userId) && "(Your turn)"}</>;
     }
     if (this.props.game.child instanceof GameEndedPhase) {
-      let gameEndedPhase = this.props.game.child;
+      const gameEndedPhase = this.props.game.child;
       if (gameEndedPhase.state.winner == 'null') {
         return <>Draw!</>;
       } else {
         return <>Winner: <b>{gameEndedPhase.state.winner}</b>!</>;
       }
     }
+  }
+  
+  renderGameGrid() {
+    return <>
+      {this.props.game.state.grid.map((row, x) => (
+        <tr key={x}>
+          {this.props.game.state.grid[x].map((cell, y) => (
+            <td key={y}
+                className={this.canFill(x, y) ? "clickable" : ""}
+                onClick={this.onCellClick.bind(this, x, y)}>
+                  {cell}
+            </td>
+          ))}
+        </tr>
+      ))}
+    </>;
   }
   
   onCellClick(x, y) {
@@ -51,6 +67,16 @@ export default class LoveLetterComponent extends React.Component {
   }
 
   canFill(x, y) {
-    return (this.props.game.state.grid[x][y] == null);
+    if (!(this.props.game.child instanceof GameInProgressPhase)) {
+      return false;
+    }
+    const gameInProgressPhase = this.props.game.child;
+    if (!gameInProgressPhase.isTurn(this.props.client.userId)) {
+      return false;
+    }
+    if (this.props.game.state.grid[x][y] != null) {
+      return false;
+    }
+    return true;
   }
 }
