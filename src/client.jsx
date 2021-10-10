3f3caedbd791713a27d250ce0765742d7b910e76
mrhappyasthma <mrhappyasthma@gmail.com>
2021-10-10 08:42:20 -0700

    Another missed rename....

--- src/client.jsx
+++ src/client.jsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { GameComponent } from "@ravens-engine/core/lib/client/index.js";
-import CoupGame from "./LoveLetterGame";
-import CoupComponent from "./LoveLetterComponent";
+import LoveLetterGame from "./LoveLetterGame";
+import LoveLetterComponent from "./components/LoveLetterComponent";
 
 ReactDOM.render(
   <GameComponent gameClass={LoveLetterGame} rootComponent={LoveLetterComponent} />,
