38207ea1517044885021dd687576da2700c61f56
mrhappyasthma <mrhappyasthma@gmail.com>
2021-10-10 10:40:47 -0700

    Set the max number of players

--- src/phases/LobbyPhase.js
+++ src/phases/LobbyPhase.js
@@ -2,6 +2,10 @@ import { Phase } from "@ravens-engine/core/lib/core/index.js";
 import GameInProgressPhase from "./GameInProgressPhase.js"
 
 export default class LobbyPhase extends Phase {
+  initialize() {
+    this.setMaxPlayers(2);
+  }
+  
   /**
    * Adds player(s) to the game during the lobby stage. Once two players
    * have been connected, the game will begin automatically.
