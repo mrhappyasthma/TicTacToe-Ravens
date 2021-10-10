e6d3dead87c6b9f3bdf714340560c00c743970ec
mrhappyasthma <mrhappyasthma@gmail.com>
2021-10-10 08:54:45 -0700

    Implement a simple lobby phase

--- src/phases/LobbyPhase.js
+++ src/phases/LobbyPhase.js
@@ -1,5 +1,22 @@
 import { Phase } from "@ravens-engine/core/lib/core/index.js";
 
-export class LobbyPhase extends Phase { }
+export class LobbyPhase extends Phase {
+  /**
+   * Adds player(s) to the game during the lobby stage. Once two players
+   * have been connected, the game will begin automatically.
+   */
+  onUserConnection(userId) {
+    this.addPlayer(userId);
+    
+    // Start the game automatically once two players have connected.
+    if (this.players.length == 2) {
+      this.parent.setChild(GameInProgressPhase);
+    }
+  }
+  
+  onUserDisconnect(userId) {
+    this.removePlayer(userId);
+  }
+}
 
 LobbyPhase.id = "lobby";
